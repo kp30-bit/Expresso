9b2cc2d293befa2c21800d9f9207b54761619cc8
Kamal Pratik <kamalpratik144@gmail.com>
2022-01-22 20:34:52 +0530

    Added my project

---
+++ app/src/main/java/com/example/orderapp/SummaryActivity.java
@@ -0,0 +1,86 @@
+package com.example.orderapp;
+
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ListView;
+
+import com.example.orderapp.Database.OrderContract;
+
+public class SummaryActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    public CartAdapter mAdapter;
+    public static final int LOADER = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_summary);
+
+
+        final Button clearthedata = findViewById(R.id.clearthedatabase2);
+        Button b=findViewById(R.id.button);
+        clearthedata.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Animation myAnim = AnimationUtils.loadAnimation(SummaryActivity.this, R.anim.bounce);
+                MyBounceInterpolator interpolator = new MyBounceInterpolator(0.2, 20);
+                myAnim.setInterpolator(interpolator);
+                clearthedata.startAnimation(myAnim);
+                int deletethedata = getContentResolver().delete(OrderContract.OrderEntry.CONTENT_URI, null, null);
+            }
+        });
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SummaryActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        getLoaderManager().initLoader(LOADER, null, this);
+
+        ListView listView = findViewById(R.id.list);
+        mAdapter = new CartAdapter(this, null);
+        listView.setAdapter(mAdapter);
+
+
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        String[] projection = {OrderContract.OrderEntry._ID,
+                OrderContract.OrderEntry.COLUMN_NAME,
+                OrderContract.OrderEntry.COLUMN_PRICE,
+                OrderContract.OrderEntry.COLUMN_QUANTITY,
+                OrderContract.OrderEntry.COLUMN_CREAM,
+                OrderContract.OrderEntry.COLUMN_HASTOPPING
+        };
+
+        return new CursorLoader(this, OrderContract.OrderEntry.CONTENT_URI,
+                projection,
+                null,
+                null,
+                null);
+
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+            mAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+        mAdapter.swapCursor(null);
+    }
+}
